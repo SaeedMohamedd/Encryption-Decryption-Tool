e3b0fa54443f4e76c326cb95b3d85914fc7a7458
SaeedMohamedd <49157131+SaeedMohamedd@users.noreply.github.com>
2019-12-09 04:33:23 +0200

    Create Feistel.java
    Add Feistel Cipher  Algorithm  Encryption and Decryption

---
+++ src/security_fxml/Feistel.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package security_fxml;
+
+/**
+ *
+ * @author saed_
+ */
+public class Feistel {
+     static public String encrypt(String text, String key, String fun) {
+
+        int n = text.length();
+        String lPart = text.substring(0, n / 2);
+        String rPart = text.substring(n / 2);
+        int x = Integer.parseInt(lPart, 2);
+        int y = Integer.parseInt(rPart, 2);
+        int k = Integer.parseInt(key, 2);
+
+        int tleft, tright;
+
+        for (int i = 0; i < 16; i++) {
+            tleft = tright = y;
+            
+
+            if (fun == "AND") {
+                tright = y & k;
+            }
+            if (fun == "OR") {
+                tright = y | k;
+            }
+            if (fun == "XOR") {
+                tright = y ^ k;
+            }
+
+            y = tright ^ x;
+            x = tleft;
+
+        }
+        int temp = y;
+        y = x;
+        x = temp;
+
+        lPart = Integer.toBinaryString(x);
+        rPart = Integer.toBinaryString(y);
+
+        if (lPart.length() < n / 2) {
+            for (int i = 0; i <= ((n / 2) - lPart.length()); i++) {
+                lPart = '0' + lPart;
+            }
+        }
+
+        if (rPart.length() < n / 2) {
+            for (int i = 0; i <= ((n / 2) - rPart.length()); i++) {
+                rPart = '0' + rPart;
+            }
+        }
+        String cipher = new String("");
+        cipher = lPart + rPart;
+        return cipher;
+    }
+
+    static public String decrypt(String text, String key, String fun) {
+        String plainText = new String("");
+        int n = text.length();
+        String lPart = text.substring(0, n / 2);
+        String rPart = text.substring(n / 2);
+        int x = Integer.parseInt(lPart, 2);
+        int y = Integer.parseInt(rPart, 2);
+        int k = Integer.parseInt(key, 2);
+
+        int tleft, tright;
+
+        for (int i = 0; i < 16; i++) {
+            tleft = tright = y;
+            
+
+            if (fun == "AND") {
+                tright = y & k;
+            }
+            if (fun == "OR") {
+                tright = y | k;
+            }
+            if (fun == "XOR") {
+                tright = y ^ k;
+            }
+
+            y = tright ^ x;
+            x = tleft;
+
+        }
+        int temp = y;
+        y = x;
+        x = temp;
+
+        lPart = Integer.toBinaryString(x);
+        rPart = Integer.toBinaryString(y);
+
+        if (lPart.length() < n / 2) {
+            for (int i = 0; i <= ((n / 2) - lPart.length()); i++) {
+                lPart = '0' + lPart;
+            }
+        }
+
+        if (rPart.length() < n / 2) {
+            for (int i = 0; i <= ((n / 2) - rPart.length()); i++) {
+                rPart = '0' + rPart;
+            }
+        }
+
+        plainText = lPart + rPart;
+        return plainText;
+    }
+
+}
+
